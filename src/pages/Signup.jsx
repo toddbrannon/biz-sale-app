2fde3574c1681a91a6bed4b6d85c046cf6b963ee
Todd Brannon <toddqbrannon@gmail.com>
2023-04-25 13:15:49 -0500

    add Signup

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>
+      <h1>SignUp Page</h1>
+      <p>This page will allow users to sign up for the app.</p>
+    </div>
+  );
+}
+
+export default SignUp;
