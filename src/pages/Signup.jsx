34663028c4ee1331f99dc79ef07a85600fc23577
Todd Brannon <toddqbrannon@gmail.com>
2023-04-25 13:28:48 -0500

    add form to signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle signup form submission here
+  };
+
   return (
     <div>
       <h1>SignUp Page</h1>
-      <p>This page will allow users to sign up for the app.</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </label>
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
   );
 }
